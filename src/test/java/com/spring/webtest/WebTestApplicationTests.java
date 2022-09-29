41d317125afe16fec501d57bb7439308a3babfa7
vogel <vogel4@bfh.ch>
2022-09-29 19:02:45 +0200

    tests fail

--- src/test/java/com/spring/webtest/WebTestApplicationTests.java
+++ src/test/java/com/spring/webtest/WebTestApplicationTests.java
@@ -3,11 +3,23 @@ package com.spring.webtest;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 @SpringBootTest
 class WebTestApplicationTests {
 
 	@Test
-	void contextLoads() {
+	void oneReturnsOneAsString() {
+		assertEquals(fizzBuzz(1), "1");
+	}
+
+	@Test
+	void integerReturnsIntegerAsString() {
+		assertEquals(fizzBuzz(7), "7");
+	}
+
+	private String fizzBuzz(int i) {
+		return "1";
 	}
 
 }
