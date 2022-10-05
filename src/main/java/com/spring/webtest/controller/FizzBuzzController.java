8a2c94d6b818717f02c0774c70c91cc85fd70cfc
vogel <vogel4@bfh.ch>
2022-10-05 09:38:39 +0200

    simple fizzbuzz frontend ready

--- src/main/java/com/spring/webtest/controller/FizzBuzzController.java
+++ src/main/java/com/spring/webtest/controller/FizzBuzzController.java
@@ -1,19 +1,23 @@
 package com.spring.webtest.controller;
 
+import com.google.gson.Gson;
 import com.spring.webtest.FizzBuzz;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import com.spring.webtest.dto.FizzBuzzDto;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class FizzBuzzController {
 
     private final FizzBuzz fb = new FizzBuzz();
+    private static final Gson gson = new Gson();
 
+    @CrossOrigin(origins = {"http://localhost:8080", "http://localhost:4200"} )
     @GetMapping("/fizzbuzz/{number}")
-    String fizzBuzz(@PathVariable int number) {
+    @ResponseBody
+    ResponseEntity<String> fizzBuzz(@PathVariable int number) {
         System.out.println("getting FizzBuzz for number :" + String.valueOf(number));
-        return fb.fizzBuzz(number);
+        return ResponseEntity.ok(gson.toJson(fb.fizzBuzz(number))) ;
     }
 
 }
---
+++ src/main/java/com/spring/webtest/dto/FizzBuzzDto.java
@@ -0,0 +1,11 @@
+package com.spring.webtest.dto;
+
+public class FizzBuzzDto {
+
+    private String fizzBuzz;
+
+    public FizzBuzzDto(String fizzBuzz) {
+        this.fizzBuzz = fizzBuzz;
+    }
+
+}
