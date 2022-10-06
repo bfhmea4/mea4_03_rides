07307b9ae0efcfa9dc3c459ea1bb55a79718acbb
Jenkis94 <julian-lanz@hotmail.com>
2022-10-06 17:44:16 +0200

    Added sample java.util.logging logger

--- src/main/java/com/spring/webtest/controller/FizzBuzzController.java
+++ src/main/java/com/spring/webtest/controller/FizzBuzzController.java
@@ -5,18 +5,23 @@ import com.spring.webtest.FizzBuzz;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.logging.Logger;
+
 @RestController
 public class FizzBuzzController {
 
     private final FizzBuzz fb = new FizzBuzz();
     private static final Gson gson = new Gson();
 
-    @CrossOrigin(origins = {"http://localhost:8080", "http://localhost:4200"} )
+    private static final Logger logger = Logger.getLogger(FizzBuzzController.class.getName());
+
+    @CrossOrigin(origins = {"http://localhost:8080", "http://localhost:4200"})
     @GetMapping("/fizzbuzz/{number}")
     @ResponseBody
     ResponseEntity<String> fizzBuzz(@PathVariable int number) {
-        System.out.println("getting FizzBuzz for number :" + String.valueOf(number));
-        return ResponseEntity.ok(gson.toJson(fb.fizzBuzz(number))) ;
+        System.out.println("getting FizzBuzz for number :" + number);
+        logger.info("getting FizzBuzz for number :" + number);
+        return ResponseEntity.ok(gson.toJson(fb.fizzBuzz(number)));
     }
 
 }
