f4cde35476809bf6494384c8c50e75a3443d96c2
vogel <vogel4@bfh.ch>
2022-10-12 13:57:11 +0200

    added User entity

---
+++ src/main/java/com/spring/webtest/database/entities/User.java
@@ -0,0 +1,12 @@
+package com.spring.webtest.database.entities;
+
+public class User {
+
+    private int id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String address;
+    private String password;
+
+}
