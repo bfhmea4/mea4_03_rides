1b5194730cf0e01c4676b8871e24b913c2d630a0
schmarcel02 <m.schmutz02@gmail.com>
2022-10-15 14:56:02 +0200

    Added RideRequest class skeletons

---
+++ src/main/java/com/spring/webtest/controller/RideRequestController.java
@@ -0,0 +1,10 @@
+package com.spring.webtest.controller;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class RideRequestController {
+    public RideRequestController() {
+
+    }
+}
