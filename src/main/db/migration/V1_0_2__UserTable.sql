e92485b8f26034b68d1f650768cf8c67a8d345a3
Gurkenblut <vogel-lukas909@hotmail.com>
2022-11-06 15:54:51 +0100

    deleted duplicated migrations login and user repository

--- src/main/db/migration/V1_0_2__UserTable.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE user (
-    `id`            INTEGER PRIMARY KEY AUTO_INCREMENT,
-    `first_name`    VARCHAR (64),
-    `last_name`     VARCHAR (64),
-    `email`         VARCHAR (64),
-    `address`       VARCHAR (128),
-    `password`      VARCHAR (32)
-)
--- src/main/ride_sharing_webbapp/src/app/login/login.component.ts
+++ src/main/ride_sharing_webbapp/src/app/login/login.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Login } from "../../model/Login";
 import {UserService} from "../../service/user.service";
 import {Router} from "@angular/router";
+import {AuthenticationService} from "../../service/authentication.service";
 
 @Component({
   selector: 'app-login',
@@ -11,6 +12,7 @@ import {Router} from "@angular/router";
 export class LoginComponent implements OnInit {
 
   constructor(private userService: UserService,
+              private authService: AuthenticationService,
               private router: Router) { }
 
   ngOnInit(): void {
@@ -22,16 +24,16 @@ export class LoginComponent implements OnInit {
       password: data.password
     }
     console.log("logging in user: " + login.email);
-    localStorage.setItem("userId", "1")
-    this.router.navigate(["/profile"]);
-    this.userService.loginUser(login).subscribe(token => {
-      if (!token) {
-        //this.router.navigate(['/login']);
-        return
+    this.authService.loginUser(login).subscribe(user => {
+      if (user) {
+        console.log("logged in User with id " + user.id + "and email:" + user.email);
+        localStorage.setItem("userId", user.id);
+        localStorage.setItem("user", JSON.stringify(user));
+        this.router.navigate(["/profile"]);
+      } else {
+        console.error("could not login user!!")
       }
-      localStorage.setItem("token", token);
-      console.log("logged in successfully");
-    });
+    })
   }
 
   navigateToRegister() {
